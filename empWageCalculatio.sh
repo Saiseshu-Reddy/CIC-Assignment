92eeadc9856480355e4cdd6add83028cd4c1c674
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-08 11:02:06 +0530

    Add User Case 1

--- empWageCalculatio.sh
+++ empWageCalculatio.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+
+#UC1
+function Attendance()
+{
+empAttendance=$((RANDOM%2))
+if [ $empAttendance -eq 0 ]
+then
+	echo "Employee is absent"
+else
+	echo "Employee is present"
+fi
+}
+Attendance
