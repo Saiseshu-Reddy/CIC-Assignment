ffcd456dc675fdfd1585bb02f8d098f19a5626d7
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-08 11:16:19 +0530

    Add User Case 2

--- empWageCalculatio.sh
+++ empWageCalculatio.sh
@@ -14,3 +14,18 @@ else
 fi
 }
 Attendance
+
+#UC2
+function DailyEmpWage()
+{
+WAGE_PER_HR=20
+FULL_DAY_HR=8
+if [ $empAttendance -eq 0 ]
+then
+	echo "Daily Employee Wage is: 0"
+else
+	dailyEmpWage=$(($WAGE_PER_HR*$FULL_DAY_HR))
+	echo "Daily Employee Wage is: $dailyEmpWage"
+fi
+}
+DailyEmpWage
